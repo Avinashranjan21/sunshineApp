9db0bd1c477c240754502c06122ee7ff9317d76f
Avinash Ranjan <Avinash.ranjan21@gmail.com>
2015-09-10 23:23:10 +0530

    code cleanup

--- app/src/main/java/com/avisoft/DetailActivityFragment.java
+++ app/src/main/java/com/avisoft/DetailActivityFragment.java
@@ -8,9 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * A placeholder fragment containing a simple view.
- */
 public class DetailActivityFragment extends Fragment {
 
     public DetailActivityFragment() {
