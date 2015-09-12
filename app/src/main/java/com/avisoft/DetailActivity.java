73df7eee8c496b896e0a59e49ce14be93b38b40d
Avinash Ranjan <Avinash.ranjan21@gmail.com>
2015-09-12 23:14:00 +0530

    Setting added to both MainActivity and Detail Activity

--- app/src/main/java/com/avisoft/DetailActivity.java
+++ app/src/main/java/com/avisoft/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.avisoft;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -30,6 +31,7 @@ public class DetailActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this,SettingsActivity.class));
             return true;
         }
 
--- app/src/main/java/com/avisoft/MainActivity.java
+++ app/src/main/java/com/avisoft/MainActivity.java
@@ -1,5 +1,6 @@
 package com.avisoft;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -55,6 +56,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this,SettingsActivity.class));
             return true;
         }
 
